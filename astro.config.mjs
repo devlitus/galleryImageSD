cc4a6a397bc938ec6d1f2cbda2487fd0b07a7ff2
devlitus <developercarles@gmail.com>
2025-01-06 10:30:28 +0100

    Agregar Sentry para el manejo de errores y un botón de prueba en la página principal

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,21 @@ import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sentry({
+      dsn: "https://82af31d911269e2fe20cb3358364e7d0@o4508578388639744.ingest.de.sentry.io/4508578394800208",
+      sourceMapsUploadOptions: {
+        project: "gallery-image",
+        authToken: import.meta.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
   adapter: vercel({
     imageService: true,
     devImageService: "sharp",
