65bdca04d2eebda8db2e4366072d7e9820800f6f
devlitus <developercarles@gmail.com>
2025-05-04 17:01:34 +0200

    Remove Sentry configuration, refactor Cloudinary setup to use constants, add global styles, and create a new constants file for environment variables.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,8 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
-
-import sentry from "@sentry/astro";
-
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [
-    tailwind(),
-    sentry({
-      sourceMapsUploadOptions: {
-        project: "gallery-image",
-        authToken: import.meta.env.SENTRY_AUTH_TOKEN,
-      },
-    }),
-  ],
-  adapter: vercel({
-    imageService: true,
-    devImageService: "sharp",
-  }),
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
---
+++ src/constants/constanst.ts
@@ -0,0 +1,4 @@
+export const CLOUDINARY_APIKEY = import.meta.env.CLOUDINARY_APIKEY;
+export const CLOUDINARY_APISECRET = import.meta.env.CLOUDINARY_APISECRET;
+export const CLOUDINARY_URL = import.meta.env.CLOUDINARY_URL;
+export const CLOUDINARY_CLOUDNAME = import.meta.env.CLOUDINARY_CLOUDNAME;
