28748dcf6198eafe2be4df1fbf8ade62a3334478
devlitus <developercarles@gmail.com>
2026-02-28 17:57:24 +0100

    refactor: wrap Vitest configuration with `defineConfig` and remove type reference directive.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
-/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
 import { getViteConfig } from 'astro/config';
 
-export default getViteConfig({
+export default getViteConfig(defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
-} as any);
+}));
